001e580c19db48eba31201fac88ba7c21c76b8b8
Matheus Barbosa de Oliveira <matheus.barbosa@dcx.ufpb.br>
2019-12-03 22:31:04 -0300

    add colors.js

---
+++ src/styles/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  colorPrimary: '#3F51B5',
+  colorBackground: '#FFFFFF',
+  textColorPrimary: '#212121',
+  textColorSecondary: '#F1F1F1',
+  colorSucess: '#00E676',
+  colorError: '#EF5350',
+};
+
+export default colors;
