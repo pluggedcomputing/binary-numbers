48f89df2f53fe4feedab6432017f75d1f445f200
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-12-06 09:59:35 -0300

    using component SlidingSwitch

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import SlidingSwitch from '../../components/slidingSwitch/index';
 import style from './styles';
 
