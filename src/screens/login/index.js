724bf74a1c49f0b5822361aa30e891faeadfed39
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-12-08 00:08:15 -0300

    added params of disblescroll , onStatusChanged and isParentScrollEnable

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -7,7 +7,16 @@ export default class index extends Component {
 	render() {
 		return (
 			<View style={style.container}>
-				<SlidingSwitch />
+				<SlidingSwitch
+					currentStatus={'login'}
+					disableScroll={(value) => {
+						console.log('scrollEnabled', value);
+					}}
+					onStatusChanged={(text) => {
+						console.log('Change Status ', text);
+					}}
+					isParentScrollEnabled={false}
+				/>
 			</View>
 		);
 	}
