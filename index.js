8e641ec5b3eb31392940d9aa3bc2a1818b80d0f8
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-12-15 12:14:59 -0300

    changed path to navigator

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './src/screens/login/index';
+import App from './src/screens/LoginOrRegister/index';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
