ced8a9ce1cad036396ea08053153e07126de9064
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-12-14 22:18:55 -0300

    created check input password in state

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,64 +1,82 @@
 import React, { Component } from 'react';
 import { View, TextInput, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import style from './styles';
+import PropTypes from 'prop-types';
+import styles from './styles';
 
 export default class index extends Component {
 	constructor(props) {
 		super(props);
-		const sizeIcon = this.props;
-		const type = this.props;
+		const { type } = props;
+		const password = type === 'password';
 		this.state = {
-			sizeIcon: sizeIcon ? sizeIcon : 32,
 			secondIcon: 'eye',
-			sizeIconPassword: type === 'password' ? sizeIcon : 0,
-			secureInput: false
+			sizeIconPassword: 32,
+			secureInput: password
 		};
 	}
 
 	getIconByType = (type) => {
 		if (type === 'password') {
-			// this.setState({ secureInput: true });
 			return 'lock';
-		} else if (type == 'email') {
-			return 'email';
-		} else {
-			return 'glass';
 		}
+
+		if (type === 'email') {
+			return 'email';
+		}
+		return 'glass';
+	};
+
+	setPassword = (type) => {
+		const secureInput = type === 'password';
+		this.setState({ secureInput });
 	};
 
 	getViewIconPassword = (type) => {
-		let view = null;
+		const { secondIcon, sizeIconPassword } = this.state;
+		let view = <Icon />;
 		if (type === 'password') {
-			view = <Icon name={this.state.secondIcon} size={this.state.sizeIconPassword} />;
-		} else {
-			view = <Icon />;
+			view = (
+				<Icon
+					name={secondIcon}
+					size={sizeIconPassword}
+					style={styles.icon}
+					onPress={(type) => console.log('trocar icone')}
+				/>
+			);
 		}
 		return view;
 	};
 
 	render() {
-		const { type, placeholder, sizeIcon } = this.props;
+		let { type, placeholder, sizeIcon } = this.props;
+
+		if (!sizeIcon) {
+			sizeIcon = 32;
+		}
 
 		return (
-			<View style={style.containerBase}>
-				<View style={style.container}>
-					<Icon
-						name={this.getIconByType(type)}
-						size={sizeIcon}
-						style={{ paddingBottom: 10, alignItems: 'flex-start' }}
-					/>
+			<View style={styles.containerBase}>
+				<View style={styles.container}>
+					<Icon name={this.getIconByType(type)} size={sizeIcon} style={styles.icon} />
 					<TextInput
+						{...this.props}
 						secureTextEntry={this.state.secureInput}
-						style={style.containerInput}
+						style={styles.containerInput}
 						placeholder={placeholder}
-						onChangeText={(type) => console.log(type)}
+						onChangeText={(text) => console.log('pegar input ', text)}
 						value={null}
 					/>
 					{this.getViewIconPassword(type)}
 				</View>
-				<Text style={style.lineBottom} />
+				<Text style={styles.lineBottom} />
 			</View>
 		);
 	}
 }
+
+index.propstype = {
+	type: PropTypes.string.isRequired,
+	sizeIcon: PropTypes.number,
+	secureInput: PropTypes.bool
+};
