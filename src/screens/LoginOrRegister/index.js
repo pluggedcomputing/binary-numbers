ca05202858bbc87ccdcbd98521ef5e2c8c5c522c
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-12-15 18:00:55 -0300

    renamed name

--- src/screens/LoginOrRegister/index.js
+++ src/screens/LoginOrRegister/index.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import SlidingSwitch from '../../components/SlidingSwitch/index';
+import Login from '../Login';
 import style from './styles';
 
 export default class index extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			status: ''
+		};
+	}
+
+	get = (status) => {
+		return status === 'login' ? <Login /> : <Text>Criar</Text>;
+	};
+
 	render() {
 		return (
 			<View style={style.container}>
 				<Image style={style.containerImage} source={require('../../assets/logo/icon.png')} />
 				<SlidingSwitch
-					currentStatus={'login'}
+					currentStatus="login"
 					disableScroll={(value) => {
 						console.log('scrollEnabled', value);
 					}}
 					onStatusChanged={(text) => {
-						console.log('Change Status ', text);
+						this.setState({ status: text });
+						console.log('Change Status ', this.state.status, 'text ', text);
 					}}
 					isParentScrollEnabled={false}
 				/>
+				{this.get(this.state.status)}
 			</View>
 		);
 	}
