8db05e0139d12044602c31e764a517cebfa21706
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-12-15 19:17:43 -0300

    use of variable containerBase

--- src/components/SlidingButton/style.js
+++ src/components/SlidingButton/style.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Dimensions } from 'react-native';
-import {fonts, metrics } from '../../styles/fonts';
+import {fonts, metrics } from '../../styles';
 
 const styles = StyleSheet.create({
 	button: {
 		flex: 1,
-		width: metrics.containerWidth / 3,
+		width: metrics.containerBase / 3,
 		height: 54,
 		justifyContent: 'center',
 		alignItems: 'center'
