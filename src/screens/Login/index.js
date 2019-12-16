9167ca58610d55f8cc3887bfeb2f5f435072edaf
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-12-15 23:15:02 -0300

    added component CustomButton

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
 import TextInput from '../../components/TextInput/index';
+import CustomButton from '../../components/CustomButton/index';
 import style from './styles';
-import styles from '../../components/TextInput/styles';
 
 export default function Login() {
 	return (
-		<View style={style.container}>
-			<TextInput type="email" placeholder="email" />
-			<Text style={styles.lineBottom} />
-			<TextInput type="password" placeholder="email" />
+		<View style={style.containerBase}>
+			<View style={style.container}>
+				<TextInput type="email" placeholder="Email" />
+				<Text style={style.lineBottom} />
+				<TextInput type="password" placeholder="Senha" />
+			</View>
+			<CustomButton text="ENTRAR" />
 		</View>
 	);
 }
