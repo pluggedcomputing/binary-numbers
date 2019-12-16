64a4f4a3ae18ba593ba9883a2832c821a055224e
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-12-15 21:24:09 -0300

    added text

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { View, Image, Text } from 'react-native';
 import TextInput from '../../components/TextInput/index';
 import style from './styles';
+import styles from '../../components/TextInput/styles';
 
 export default function Login() {
 	return (
 		<View style={style.container}>
 			<TextInput type="email" placeholder="email" />
+			<Text style={styles.lineBottom} />
 			<TextInput type="password" placeholder="email" />
 		</View>
 	);
