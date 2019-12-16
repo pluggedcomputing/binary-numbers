73057b316121cefb991cb5dcb0d72ea781fb8f3d
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-12-16 17:39:03 -0300

    added property elevation

--- src/components/CustomButton/styles.js
+++ src/components/CustomButton/styles.js
@@ -1,21 +1,22 @@
-import {StyleSheet} from 'react-native';
-import {colors, metrics, fonts} from '../../styles';
+import { StyleSheet } from 'react-native';
+import { colors, metrics, fonts } from '../../styles';
 
 const styles = StyleSheet.create({
-  container: {
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: colors.colorPrimary,
-    height: 65,
-    width: metrics.screenWidth / 2,
-  },
-  textButton: {
-    fontSize: fonts.title,
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    color: colors.textColorSecondary,
-  },
+	container: {
+		borderRadius: 5,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: colors.colorPrimary,
+		height: 65,
+		width: metrics.screenWidth / 2,
+		elevation: 4
+	},
+	textButton: {
+		fontSize: fonts.title,
+		fontWeight: 'bold',
+		textTransform: 'uppercase',
+		color: colors.textColorSecondary
+	}
 });
 
 export default styles;
