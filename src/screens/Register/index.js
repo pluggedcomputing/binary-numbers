980338e56ad7d4ca4b2d47b85a91c747bba63194
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-12-18 18:19:41 -0300

    create component register

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import TextInput from '../../components/TextInput';
+import CustomButton from '../../components/CustomButton';
+import style from './styles';
 
 export default function Register() {
 	return (
-		<View>
-			<Text>asdfasdf</Text>
+		<View style={style.containerBase}>
+			<View style={style.containerBody}>
+				<TextInput type="account" placeholder="Nome" sizeIcon={32} />
+				<Text style={style.lineBottom} />
+				<TextInput type="email" placeholder="Email" />
+				<Text style={style.lineBottom} />
+				<TextInput type="password" placeholder="Senha" />
+				<Text style={style.lineBottom} />
+				<TextInput type="password" placeholder="Confirmação" />
+			</View>
+			<CustomButton text="CADASTRAR" />
 		</View>
 	);
 }
