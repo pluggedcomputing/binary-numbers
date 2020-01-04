db60ac64af32f1b08f99470bc7fa4bef2d31106b
Matheus Barbosa de Oliveira <matheus.barbosa@dcx.ufpb.br>
2020-01-04 13:30:28 -0300

    Melhorias no componente CustomBackgound

--- src/components/CustomBackground/index.js
+++ src/components/CustomBackground/index.js
@@ -1,52 +1,47 @@
-import React, { useState } from 'react';
-import { Image, View, TouchableOpacity, Text } from 'react-native';
+import React, {useState} from 'react';
+import {Image, View, TouchableOpacity, Text} from 'react-native';
 import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-import arrowNext from '../../assets/next.png'
-import arrowPrevious from '../../assets/previous.png'
-
+import arrowNext from '../../assets/images/next.png';
+import arrowPrevious from '../../assets/images/previous.png';
 
 const CustomBackground = props => {
-  const { content } = props;
-  const [page, setPage] = useState(0)
-  const contentSize = content.length - 1
+  const {content} = props;
+  const [page, setPage] = useState(0);
+  const contentSize = content.length - 1;
 
   function nextPage() {
-    if(page < contentSize) {
-      setPage(page+1)
+    if (page < contentSize) {
+      setPage(page + 1);
     }
   }
   function previousPage() {
     if (page > 0) {
-      setPage(page - 1)
+      setPage(page - 1);
     }
   }
 
   return (
-
     <View style={styles.container}>
-      <View style={[styles.container, { transform: [{ rotate: '-3deg' }] }]}>
-        <View style={[styles.contain, { transform: [{ rotate: '2deg' }] }]}>{content[page]}</View>
-        <View style={[styles.loadButton, { transform: [{ rotate: '3deg' }] }]}>
-          <TouchableOpacity
-            onPress={previousPage}
-            style={styles.button}
-          >
+      <View style={[styles.container, {transform: [{rotate: '-3deg'}]}]}>
+        <View style={styles.content}>{content[page]}</View>
+
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity onPress={previousPage}>
             <Image style={styles.Icon} source={arrowPrevious} />
           </TouchableOpacity>
-          <TouchableOpacity
-            onPress={nextPage}
-            style={styles.button}
-          >
+          <TouchableOpacity onPress={nextPage}>
             <Image style={styles.Icon} source={arrowNext} />
           </TouchableOpacity>
         </View>
-      </View>
-      <View><Text style={styles.textCont}>{page + 1}/{content. length}</Text></View>
-    </View>
 
+        <Text style={styles.textCont}>
+          {page + 1}/{content.length}
+        </Text>
+      </View>
+    </View>
   );
 };
 
--- src/components/CustomBackground/styles.js
+++ src/components/CustomBackground/styles.js
@@ -1,48 +1,44 @@
 import {StyleSheet} from 'react-native';
 import {colors, metrics} from '../../styles';
 
-const WIDHT_PORCENTAGE = 0.9
-const CENTER_PORCENTAGE = 0.6
-const HEIGHT_PORCENTAGE = 0.333
-const MARGINLEFT_TEXT = 260
-
+const WIDHT_PORCENTAGE = 0.9;
+const HEIGHT_PORCENTAGE = 0.3;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     borderWidth: 1.5,
     borderRadius: 5,
     borderColor: colors.textcolorPrimary,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: colors.textColorSecondary,
+    backgroundColor: colors.colorBackground,
     height: metrics.screenHeight * HEIGHT_PORCENTAGE,
     width: metrics.screenWidth * WIDHT_PORCENTAGE,
-    marginTop: metrics.screenWidth * CENTER_PORCENTAGE,
-    position: "absolute"
   },
-  loadButton: {
+  content: {
+    flex: 3,
+    padding: 10,
+    transform: [{rotate: '2deg'}],
+  },
+  buttonsContainer: {
+    flex: 0.5,
+    marginTop: 5,
     flexDirection: 'row',
-    position: 'absolute'
+    justifyContent: 'flex-end',
   },
   Icon: {
     width: 20,
-    height:20,
-    resizeMode: 'contain',
-  },
-  button: {
-    marginHorizontal: 4,
-    marginTop: 160
-  },
-  contain: {
-    padding: 10,
+    height: 20,
+    margin: 5,
     resizeMode: 'contain',
   },
   textCont: {
-    alignContent: 'flex-end',
-    marginTop: 160,
-    marginLeft: MARGINLEFT_TEXT,
-  }
+    flex: 0.5,
+    color: colors.textColorPrimary,
+    fontWeight: 'bold',
+    alignSelf: 'flex-end',
+    padding: 5,
+  },
 });
 
 export default styles;
