a6d073a24d375bfc3a693ca00a5942f5c482cec3
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2020-01-24 21:19:22 -0300

    Ajustando o style

--- src/screens/Congratulations/index.js
+++ src/screens/Congratulations/index.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { ProgressBar,SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
+import { ProgressBar } from 'react-native-paper';
 
 import Lottie from 'lottie-react-native';
-import {PropTypes} from 'prop-types';
+import { PropTypes } from 'prop-types';
 
 import animation from '../../assets/animations/CheckedDone.json'
-import {colors} from '../../styles';
+import { colors } from '../../styles';
 import styles from './styles';
+
 
 const Congratulations = props => {
   const { level } = props
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Parabéns</Text>
+      <Text style={styles.text1}>Parabéns!!!</Text>
       <Lottie source={animation} autoPlay loop />
-      <Text style={styles.text}>Você completou o nível X{level}</Text>
-      <ProgressBar progress={1} color={colors.colorSucess} />
+      <Text style={styles.text2}>Você completou o nivel {level}</Text>
+      <ProgressBar color={colors.colorSucess} progress={1} />
     </SafeAreaView>
   );
 }
 
 Congratulations.propTypes = {
-  level: PropTypes.string.isRequired,
+  level: PropTypes.number.isRequired,
 }
 
 export default Congratulations;
--- src/screens/Congratulations/styles.js
+++ src/screens/Congratulations/styles.js
@@ -6,15 +6,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: metrics.screenWidth,
-    height: metrics.screenHeight,
+    height: metrics.screenHeight ,
     backgroundColor: 'rgba(0, 0, 0, 0.8)',
   },
-  text: {
-    color: colors.colorBackground,
-    fontSize: 50,
+  text1: {
+    flex: 1,
+    textAlign: 'center',
+    color: colors.textColorSecondary,
+    fontSize: metrics.screenWidth * 0.14,
     fontWeight: 'bold',
-    margin: metrics.smallMargin,
-  }
+    justifyContent: 'flex-start',
+    margin: metrics.baseMargin,
+    paddingVertical: 150
+  },
+  text2: {
+    flex: 1,
+    textAlign: 'center',
+    color: colors.textColorSecondary,
+    fontSize: metrics.screenWidth * 0.1,
+    fontWeight: 'bold',
+    justifyContent: 'flex-end',
+    margin: metrics.baseMargin,
+  },
 });
 
 export default styles;
