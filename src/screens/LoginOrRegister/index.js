983931b59ca2e481e1168f750134b7110e7b22ad
Matheus Barbosa de Oliveira <matheus.barbosa@dcx.ufpb.br>
2020-02-17 21:48:53 -0300

    fix(#35): adicionado margintop na tela de login

--- src/screens/LoginOrRegister/index.js
+++ src/screens/LoginOrRegister/index.js
@@ -1,63 +1,46 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {ScrollView, Image, StatusBar, KeyboardAvoidingView} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
+import image from '../../assets/images/logo_grey.png';
 import SlidingSwitch from '../../components/SlidingSwitch';
-import {colors, metrics} from '../../styles';
+import {colors} from '../../styles';
 import Login from './Login';
 import Register from './Register';
 import styles from './styles';
 
-const textLogin = 'Entrar';
+const LoginOrRegister = props => {
+  const [status, setStatus] = useState('');
+  const {navigation} = props;
 
-export default class index extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: '',
-    };
-  }
+  return (
+    <LinearGradient
+      colors={[colors.colorPrimary, '#242F68']}
+      style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={colors.colorPrimary}
+      />
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        showsVerticalScrollIndicator={false}>
+        <Image style={styles.containerImage} source={image} />
+        <KeyboardAvoidingView behavior="padding" enabled>
+          <SlidingSwitch
+            onStatusChanged={text => {
+              setStatus(text);
+            }}
+            isParentScrollEnabled={false}
+          />
+          {status === 'Entrar' ? (
+            <Login navigation={navigation} />
+          ) : (
+            <Register navigation={navigation} />
+          )}
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </LinearGradient>
+  );
+};
 
-  getContainerLoginOrRegister = status => {
-    const {navigation} = this.props;
-    return status === textLogin ? (
-      <Login navigation={navigation} />
-    ) : (
-      <Register navigation={navigation} />
-    );
-  };
-
-  render() {
-    const {status} = this.state;
-    const image = require('../../assets/images/logo_grey.png');
-
-    return (
-      <LinearGradient
-        colors={[colors.colorPrimary, '#242F68']}
-        style={styles.container}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor={colors.colorPrimary}
-        />
-        <ScrollView
-          contentContainerStyle={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: metrics.screenWidth,
-          }}
-          showsVerticalScrollIndicator={false}>
-          <Image style={styles.containerImage} source={image} />
-          <KeyboardAvoidingView behavior="padding" enabled>
-            <SlidingSwitch
-              onStatusChanged={text => {
-                this.setState({status: text});
-              }}
-              isParentScrollEnabled={false}
-            />
-            {this.getContainerLoginOrRegister(status)}
-          </KeyboardAvoidingView>
-        </ScrollView>
-      </LinearGradient>
-    );
-  }
-}
+export default LoginOrRegister;
