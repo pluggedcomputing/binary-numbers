8b6d6b4fff1bc3a0cf24ca6f58b3c218cf597ba2
Matheus Barbosa de Oliveira <matheus.barbosa@dcx.ufpb.br>
2020-03-02 22:35:27 -0300

    chore: ajustes no projeto

--- __tests__/AlphabetTable-test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import AlphabetTable from '../src/components/AlphabetTable/index';
-
-test('renders correctly', () => {
-  const tree = renderer.create(<AlphabetTable />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
