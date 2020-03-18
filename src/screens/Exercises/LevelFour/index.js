ba404ec194b6c1541d16f657242f295147263fd8
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-03-18 12:25:49 -0300

    feat(#39): adicionar step para componete christmansTree

--- src/screens/Exercises/LevelFour/index.js
+++ src/screens/Exercises/LevelFour/index.js
@@ -21,29 +21,39 @@ export default function LevelFour({navigation}) {
   const finishLevel = step === finalPage;
   const messagePlaceholder = 'Digite aqui';
   const iconName = 'square-edit-outline';
-  // let numberSelection = '';
 
-  const [arrayRow, setArrayRow] = useState([
+  const instanceInitial = [
     {id: 1, value: ''},
     {id: 2, value: ''},
     {id: 3, value: ''},
     {id: 4, value: ''},
     {id: 5, value: ''},
-  ]);
+  ];
+
+  const [numbers, setNumbers] = useState('');
+
+  const [arrayRow, setArrayRow] = useState(instanceInitial);
+
+  const [componentChristmasTree, setComponentChristmasTree] = useState(
+    <ChristmasTreeTableStepOne />,
+  );
 
   useEffect(() => {
-    if (finishLevel) navigation.navigate('Congratulations');
+    if (finishLevel) {
+      navigation.navigate('Congratulations');
+    } else if (step === 2) {
+      setComponentChristmasTree(<ChristmasTreeTableStepTwo />);
+    } else if (step === 3) {
+      setComponentChristmasTree(<ChristmasTreeTableStepThree />);
+    }
+    // else if (step === 4) {
+    //   setComponentChristmasTree(
+    //     <Text style={{flex: 1, backgroundColor: colors.colorBackground}}>
+    //       teasdfxto{numbers}
+    //     </Text>,
+    //   );
+    // }
   }, [step]);
-
-  function changeChristmasTree(acutalStep) {
-    const tree = {
-      1: <ChristmasTreeTableStepOne />,
-      2: <ChristmasTreeTableStepTwo />,
-      3: <ChristmasTreeTableStepThree />,
-    };
-
-    return tree[acutalStep];
-  }
 
   function updateDataInput(index, text) {
     const newArray = [...arrayRow];
@@ -52,21 +62,35 @@ export default function LevelFour({navigation}) {
     setArrayRow(newArray);
   }
 
-  return (
-    <ScrollView>
-      <View style={styles.container}>
-        <HeaderOfExercises
-          title=" Nível 4 - Enviar Mensagens Secretas"
-          navigation={navigation}
-        />
-        <View style={styles.tree}>{changeChristmasTree(step)}</View>
+  function checkEmpytInputs() {
+    if (step < 4) {
+      for (let index = 0; index < arrayRow.length; index += 1) {
+        if (arrayRow[index].value === '') return false;
+      }
+    }
+    return true;
+  }
+
+  function insertListNumber() {
+    let numberSelection = numbers;
+    arrayRow.forEach(item => {
+      numberSelection += `${item.value} `;
+    });
+    if (step < 3) numberSelection += '-';
+    setNumbers(numberSelection);
+  }
+
+  function christmansTree() {
+    return (
+      <View style={styles.componetTree}>
+        <View style={styles.tree}>{componentChristmasTree}</View>
         <KeyboardAvoidingView
           style={styles.subContainer}
           behavior="padding"
           enabled>
           {arrayRow.map(item => (
-            <View style={styles.rowContainer}>
-              <Text key={item.id}>Linha {item.id} =</Text>
+            <View key={item.id} style={styles.rowContainer}>
+              <Text>Linha {item.id} =</Text>
               <CustomTextInput
                 style={styles.input}
                 keyboardType="numeric"
@@ -78,14 +102,37 @@ export default function LevelFour({navigation}) {
             </View>
           ))}
         </KeyboardAvoidingView>
+      </View>
+    );
+  }
+
+  function changeComponent(acutalStep) {
+    if (acutalStep < 4) {
+      return christmansTree();
+    }
+    return (
+      <Text style={{flex: 1, backgroundColor: colors.colorBackground}}>
+        {numbers}
+      </Text>
+    );
+  }
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <HeaderOfExercises
+          title=" Nível 4 - Enviar Mensagens Secretas"
+          navigation={navigation}
+        />
+        {changeComponent(step)}
         <ChoiceButton
           text="Enviar"
-          correct
           onPress={() => {
-            setSteps(step + 1);
-            // if (item.correct) {
-            //   setSteps(step + 1);
-            // }
+            if (checkEmpytInputs() || step > 3) {
+              insertListNumber();
+              setSteps(step + 1);
+              setArrayRow(instanceInitial);
+            }
           }}
         />
         <ProgressBar
