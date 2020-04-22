ed42101ccf2f92d5c4dc1f7cd3b885c4835935a1
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-04-21 23:59:55 -0300

    feat(#47): adicionar uso de route para pegar o parametro level

--- src/screens/Congratulations/index.js
+++ src/screens/Congratulations/index.js
@@ -1,36 +1,41 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView, Text, StatusBar, TouchableOpacity } from 'react-native';
-import { ProgressBar } from 'react-native-paper';
+import React, {useEffect} from 'react';
+import {SafeAreaView, Text, StatusBar, TouchableOpacity} from 'react-native';
+import {ProgressBar} from 'react-native-paper';
 
+import {useRoute} from '@react-navigation/native';
 import Lottie from 'lottie-react-native';
 
-import animation from '../../assets/animations/CheckedDone.json'
-import { colors } from '../../styles';
+import animation from '../../assets/animations/CheckedDone.json';
+import {colors} from '../../styles';
 import styles from './styles';
 
-
 const Congratulations = props => {
-  const { navigation } = props
-  const level = navigation.getParam('level', 'X')
-  const navigateScreen = () => navigation.navigate('LevelSelection')
+  const {navigation} = props;
+  const {level} = useRoute().params;
 
-  useEffect(() => { setTimeout(navigateScreen, 3000); }, []);
+  const navigateScreen = () => navigation.navigate('LevelSelection');
+
+  useEffect(() => {
+    setTimeout(navigateScreen, 3000);
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar
-        barStyle='light-content'
+        barStyle="light-content"
         backgroundColor="rgba(0, 0, 0, 0.8)"
       />
       <Text style={styles.textTop}>Parabéns!!!</Text>
       <Lottie source={animation} autoPlay loop />
       <Text style={styles.textEnd}>Você completou o nível {level}</Text>
-      <TouchableOpacity style={styles.buttonAltenative} onPress={navigateScreen}>
+      <TouchableOpacity
+        style={styles.buttonAltenative}
+        onPress={navigateScreen}>
         <Text style={styles.textButton}>Continuar </Text>
       </TouchableOpacity>
       <ProgressBar color={colors.colorSucess} progress={1} />
     </SafeAreaView>
   );
-}
+};
 
 export default Congratulations;
