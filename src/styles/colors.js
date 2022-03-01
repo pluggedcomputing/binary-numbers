9adfd754ae8c2bf95938c05b6e177b54c3e87fdb
Marcos-Vinicius9 <viniciusferreirag213@gmail.com>
2022-03-01 19:30:09 -0300

    feat(#17): paleta de cores atualizada

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -2,8 +2,9 @@ const colors = {
   colorPrimary: '#3F51B5',
   colorBackground: '#FFFFFF',
   colorBackgroundSecondary:'#90CCFF',
+  colorBackgroundTertiary:'#E5E5E5',
   textColorPrimary: '#07377A',
-  textColorSecondary: '#FFFFFF',
+  textColorSecondary: '#F1F1F1',
   colorSucess: '#00E676',
   colorError: '#EF5350',
 };
