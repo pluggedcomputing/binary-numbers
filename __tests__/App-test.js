6f2d5229c84c9751c6c2b6f8ac5d9b7a1c38c04c
Marcos Vinicius <viniciusferreirag213@gmail.com>
2023-03-04 14:31:59 -0300

    feat(#53): implementando testes jest

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,11 +5,21 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
-
-import App from '../App';
+import ScreenAbout from '../src/screens/ScreenAbout'
+import CardLevel from '../src/components/CardLevel';
+import CardGroup from '../src/components/CardGroup';
+import ModalAlert from '../src/components/ModalAlert';
 
 // Note: test renderer must be required after react-native.
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+describe('renders correctly', () =>{
+
+  test('renders correctly', () => {
+    renderer.create(<ScreenAbout/>);
+  });
+  
+  test('the component rendered', () => {
+    renderer.create(<CardGroup/>);
+  });
+})
+
