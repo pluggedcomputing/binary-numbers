e9c1e26316e8ffa9e07393ca3f87080dcdb1a3b0
Marcos Vinicius <viniciusferreirag213@gmail.com>
2023-03-04 14:36:26 -0300

    feat(#53): implementando testes jest

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,17 +9,25 @@ import ScreenAbout from '../src/screens/ScreenAbout'
 import CardLevel from '../src/components/CardLevel';
 import CardGroup from '../src/components/CardGroup';
 import ModalAlert from '../src/components/ModalAlert';
+import CustomTextInput from '../src/components/CustomTextInput';
 
 // Note: test renderer must be required after react-native.
 
 describe('renders correctly', () =>{
 
-  test('renders correctly', () => {
+  test('the component rendered', () => {
     renderer.create(<ScreenAbout/>);
   });
   
   test('the component rendered', () => {
-    renderer.create(<CardGroup/>);
+    renderer.create(<CustomTextInput
+      keyboardType="numeric"
+      placeholder="Digite aqui.."
+      onChangeText={text => setNumericAnswer(text)}
+      value={1}
+    />);
   });
 })
+
+
 
