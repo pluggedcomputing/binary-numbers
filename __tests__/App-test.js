48c5c801f6d6ce7f98ac5bdbfe80477fe47a92c3
Marcos Vinicius <viniciusferreirag213@gmail.com>
2023-03-05 14:51:16 -0300

    feat#53):inserindo test de props

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,11 +5,11 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {render, screen} from '@testing-library/react-native';
+
 import ScreenAbout from '../src/screens/ScreenAbout'
-import CardLevel from '../src/components/CardLevel';
-import CardGroup from '../src/components/CardGroup';
-import ModalAlert from '../src/components/ModalAlert';
 import CustomTextInput from '../src/components/CustomTextInput';
+import CustomButton from '../src/components/CustomButton';
 
 // Note: test renderer must be required after react-native.
 
@@ -18,9 +18,18 @@ describe('renders correctly', () =>{
   test('the component rendered', () => {
     renderer.create(<ScreenAbout/>);
   });
-  
+
+  test('verificando se o props text está funcionando', () =>{
+    render(<CustomButton
+    text = "test"
+    /> )
+
+    const buttonText = screen.getByText("test");
+    
+  })
+
   test('the component rendered', () => {
-    renderer.create(<CustomTextInput
+    render(<CustomTextInput
       keyboardType="numeric"
       placeholder="Digite aqui.."
       onChangeText={text => setNumericAnswer(text)}
